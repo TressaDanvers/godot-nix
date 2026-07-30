9670f9e351959a4570dc1c00787fc5fb13bab46c
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-30 14:50:33 -0400

    removed unneeded inputs

--- overlays.nix
+++ overlays.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: final: prev: {
+final: prev: {
   mkGodot = prev.callPackage ./mkGodot.nix { };
   mkNixosPatch = prev.callPackage ./mkNixosPatch.nix { };
   export_templates = prev.callPackage ./export_templates.nix { };
