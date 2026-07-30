7085255f148e4532db61c3c68a99bc3828d9aeec
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-30 14:44:26 -0400

    setup overlays

---
+++ overlays.nix
@@ -0,0 +1,5 @@
+{ inputs, ... }: final: prev: {
+  mkGodot = prev.callPackage ./mkGodot.nix { };
+  mkNixosPatch = prev.callPackage ./mkNixosPatch.nix { };
+  export_templates = prev.callPackage ./export_templates.nix { };
+}
